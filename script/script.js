f662dea3604a0a72370d7d57fddabe4d4e6d67a1
Renan Luiz <renanluizssx@gmail.com>
2022-11-25 21:29:55 -0300

    commitado

--- script/script.js
+++ script/script.js
@@ -1,14 +1,51 @@
 // const usasse quando uma variável não vai receber novos valores
+//GetElementsByClassname sempre vai pegar uma coleção
+//ctrl d altera tudo
+// crtl shift l pega tudo
+//f2 altera tudo 
+//localStorage não atende o array
+//transforme array por json pra ele identificar como se fosse um array
+// boa prática não usar uma função dentro da outra
+// a partir do momento que usamos um return, abaixo morre;
+// return pode ser usado somente uma vez dentro de uma função;
+//tentei usar o required e não funcionou
+// a partir do momento que colocou o event ele travou
+//localstorage não reconhece array
+let formulario = document.getElementById("form");
+formulario.addEventListener("submit", AdicionarTarefa)
 let tarefas = document.getElementById('tarefas');
 let nome = document.getElementById('nome');
 let divquantidade = document.getElementById('quantidades');
-divplus = 0;
 let apagar = 0;
 let check = [];
-controle = 0;
-const getBanco = () => JSON.parse(localStorage.getItem('todo')) ?? [];
-const setBanco = (banco) => localStorage.setItem('todo', JSON.stringify(banco))
-// return pode ser usado somente uma vez dentro de uma função;
+let banco = [];
+function AdicionarTarefa(event) {
+  event.preventDefault()
+  controle = document.getElementsByClassName("minhas-tarefas").length + 1
+ if (nome.value == "") {
+    window.alert("Você precisa adicionar uma tarefa");
+    return null;
+    
+    
+  }
+divquantidade.innerHTML = ``;
+apagar++;
+let tarefa = `<div class="minhas-tarefas" id="minhas-tarefas-${controle}">
+                <span>${nome.value}</span>
+                <input class="checkbox" onclick="ConcluirTarefa(${controle})" id="checkbox-${controle}" type="checkbox">
+            </div>` 
+// na primeira vez que ele pegar ele vai retornar null
+  //console.log(local);
+            
+  
+  check.push(tarefa)
+            
+  
+            nome.value = "";
+tarefas.innerHTML +=tarefa
+
+
+}
   const quantidade = (c) => {
         if (c == 0) {
           return divquantidade.innerHTML = `<div class="quantidade">${c}</div>`;
@@ -18,89 +55,31 @@ const setBanco = (banco) => localStorage.setItem('todo', JSON.stringify(banco))
         return divquantidade.innerHTML = `<div class="quantidade">${check.length}</div>`;
       }
   }
-function ConcluirTarefa(divplus) {
-  console.log(divplus)
-  const fundocheckbox = document.getElementById(`minhas-tarefas-${divplus}`);
-  const checkbox = document.getElementById(`checkbox-${divplus}`);
-  const audio = new Audio('audio/Áudio.mp3')
-  //console.log(divplus)
-  if (checkbox.checked) {
-    fundocheckbox.classList.add("cor");
-    audio.play();
-                
-
-  }
-  else {
-    fundocheckbox.classList.remove("cor");
-  }
-  
-
-}
-// função pode ser
-function AdicionarTarefa() {
-  // boa prática não usar uma função dentro da outra
-  
-  if (nome.value == "") {
-    window.alert("Você precisa adicionar uma tarefa");
-    return null;
-    
-    // a partir do momento que usamos um return, abaixo morre;
-  }
-  
-  
-divquantidade.innerHTML = ``;
-apagar++;
-divplus++; 
-  
-
-
-let tarefa = `<div class="minhas-tarefas" id="minhas-tarefas-${divplus}">
-                <span>${nome.value}</span>
-                <input class="checkbox" onclick="ConcluirTarefa(${divplus})" id="checkbox-${divplus}" type="checkbox">
-            </div>` 
-            check.push(tarefa)
-            
-
-
-
-//tarefas.innerHTML +=tarefa
-/*
-const addtarefa = () => {
-  const banco = getBanco();
-  banco.push({tarefa: nome.value})
-  setBanco(banco)
-  const banco2 = getBanco();
-  banco2.forEach(() => {
-     AdicionarTarefa(nome.value)
-  })
-addtarefa();
-*/
-}     
-
-  
- // localStorage.setItem("tarefa", check[controle])
-  //controle++;
- //tarefas.innerHTML = localStorage.getItem("tarefa")
-
-
-localStorage.setItem("minhastarefas",`<div class="minhas-tarefas" id="minhas-tarefas-${divplus}">
-<span>comer</span>
-<input class="checkbox" onclick="ConcluirTarefa()" id="checkbox-${divplus}" type="checkbox">
-</div>`)
-controle++;
-tarefas.innerHTML += localStorage.getItem("minhastarefas")
-
-
-//localStorage.tarefas = "check";
-//controle++
-//tarefas.innerHTML += localStorage.tarefas;
 
 
   const excluir = () =>  {
     tarefas.innerHTML = ``;
-    divplus = 0;
     check.splice(0, apagar);
     apagar = 0;
     return quantidade(0);
   }
+  function ConcluirTarefa(controle) {
+    const fundocheckbox = document.getElementById(`minhas-tarefas-${controle}`);
+    const checkbox = document.getElementById(`checkbox-${controle}`);
+    const audio = new Audio('audio/Áudio.mp3')
+    //console.log(divplus)
+    if (checkbox.checked) {
+      fundocheckbox.classList.add("cor");
+      audio.play();
+                  
+  
+    }
+    else {
+      fundocheckbox.classList.remove("cor");
+    }
+    
+  
+  }
+
+
 
