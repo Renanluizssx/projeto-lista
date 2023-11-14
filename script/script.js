c51302e98c9839acc730896a1721cbfe26ab686c
Renanluizssx <renanluizssx@gmail.com>
2023-11-14 11:00:30 -0300

    commitado

--- script/script.js
+++ script/script.js
@@ -1,81 +1,66 @@
-document.addEventListener("DOMContentLoaded", pegarTarefa)
-let formulario = document.getElementById("form");
-formulario.addEventListener("submit", adicionarTarefa)
-let tarefas = document.getElementById('tarefas');
+document.addEventListener("DOMContentLoaded", pegarTarefa);
+const formulario = document.getElementById("form");
+formulario.addEventListener("submit", adicionarTarefa);
+const tarefas = document.getElementById("tarefas");
 
-let getItem = (chave) => JSON.parse(localStorage.getItem(chave));
-let setItem = (chave, valor) => localStorage.setItem(chave, JSON.stringify(valor));
-  
-let printarTarefa = (tarefa) => {
-  
-  index = document.getElementsByClassName("minhas-tarefas").length
-  console.log(document.getElementsByClassName("minhas-tarefas"))
+const getItem = (chave) => JSON.parse(localStorage.getItem(chave));
+const setItem = (chave, valor) =>
+  localStorage.setItem(chave, JSON.stringify(valor));
+
+const printarTarefa = (tarefa) => {
+  const index = document.getElementsByClassName("minhas-tarefas").length;
+
   if (tarefa === "") {
-     window.alert("Você precisa adicionar uma tarefa");
-    return null; 
-   }
-   
-  let tarefastring = 
-      `<div class="minhas-tarefas" id="minhas-tarefas-${index}">
+    window.alert("Você precisa adicionar uma tarefa");
+    return null;
+  }
+
+  const tarefastring = `<div class="minhas-tarefas" id="minhas-tarefas-${index}">
       <span>${tarefa}</span>
-      <input type="checkbox" class="checkbox" onclick="ConcluirTarefa(${index})" id="checkbox-${index}" >
-      <button id="excluir" class="excluir" onclick="ExcluirTarefa(${index})">Excluir</button>
-      </div>` 
-    
-    tarefas.innerHTML +=tarefastring;
-    
-    
-}
+      <input type="checkbox" class="checkbox" onclick="concluirTarefa(${index})" id="checkbox-${index}" >
+      <button id="excluir" class="excluir" onclick="excluirTarefa(${index})">Excluir</button>
+      </div>`;
+
+  tarefas.innerHTML += tarefastring;
+};
 function adicionarTarefa(evento) {
-  
-  evento.preventDefault()
-  let nome = document.getElementById('nome');
-  
-  printarTarefa(nome.value)
-  
-  
-  local = getItem("local");
-  
-  
+  evento.preventDefault();
+
+  const nome = document.getElementById("nome");
+
+  printarTarefa(nome.value);
+
+  const local = getItem("local");
+
   if (local === null) {
-    return setItem("local", [nome.value])
+    setItem("local", [nome.value]);
+    nome.value = "";
   }
-  return setItem("local", [...local, nome.value])
-  
+
+  setItem("local", [...local, nome.value]);
+  nome.value = "";
 }
-  function ConcluirTarefa(index) {
-    const fundocheckbox = document.getElementById(`minhas-tarefas-${index}`);
-    const checkbox = document.getElementById(`checkbox-${index}`);
-    const audio = new Audio('audio/Áudio.mp3')
-    if (checkbox.checked) {
-      fundocheckbox.classList.add("cor");
-      audio.play();
-                  
-  
-    }
-    else {
-      fundocheckbox.classList.remove("cor");
-    }
-    
-  
+function concluirTarefa(index) {
+  const fundocheckbox = document.getElementById(`minhas-tarefas-${index}`);
+  const checkbox = document.getElementById(`checkbox-${index}`);
+  const audio = new Audio("audio/Áudio.mp3");
+  if (checkbox.checked) {
+    fundocheckbox.classList.add("cor");
+    audio.play();
+  } else {
+    fundocheckbox.classList.remove("cor");
   }
-  function pegarTarefa () {
-    
-    let local = getItem("local");
-    if (local === null) {
-      return null
-    }
-    local.forEach((tarefa) => printarTarefa(tarefa));
-
-  }
-  function ExcluirTarefa(excluir) {
-
-    local = getItem("local");
-    local.splice(excluir, 1);
-    setItem("local", local);
-    location.reload()
-  
 }
-
-
-
+function pegarTarefa() {
+  const local = getItem("local");
+  if (local === null) {
+    return null;
+  }
+  local.forEach((tarefa) => printarTarefa(tarefa));
+}
+function excluirTarefa(excluir) {
+  const local = getItem("local");
+  local.splice(excluir, 1);
+  setItem("local", local);
+  location.reload();
+}
